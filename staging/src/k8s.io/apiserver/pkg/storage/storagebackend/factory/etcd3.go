8da00ac372677c17b7c8344d5507c0b479336fd0
Eugene Sergeichyk <eugene.sergeichyk@datadoghq.com>
2025-10-11 16:56:52 -0400

    Making RelaxedDNSSearchValidation=false

--- staging/src/k8s.io/apiserver/pkg/storage/storagebackend/factory/etcd3.go
+++ staging/src/k8s.io/apiserver/pkg/storage/storagebackend/factory/etcd3.go
@@ -319,12 +319,12 @@ var newETCD3Client = func(c storagebackend.TransportConfig) (*kubernetes.Client,
 		grpc.WithDefaultServiceConfig(`{
   "load_balancing_config": [{
     "outlier_detection": {
-      "interval": "2s",
+      "interval": "10s",
       "base_ejection_time": "30s",
       "max_ejection_time": "300s",
-      "max_ejection_percent": 10,
+      "max_ejection_percent": 50,
       "failure_percentage_ejection": {
-        "threshold": 85,
+        "threshold": 20,
         "enforcement_percentage": 100,
         "minimum_hosts": 3,
         "request_volume": 5
